bbc7ad8aff2554101226ba31f10e1f9a10f32244
Felix Tröbinger <felix.troebinger@gmail.com>
2019-12-10 14:00:55 +0100

    wonky working formatting buttons

--- app/src/main/java/at/fhooe/mc/android/mare/ui/editor/EditorFragment.java
+++ app/src/main/java/at/fhooe/mc/android/mare/ui/editor/EditorFragment.java
@@ -1,12 +1,14 @@
 package at.fhooe.mc.android.mare.ui.editor;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,13 +22,12 @@ import at.fhooe.mc.android.mare.EditorActivity;
 import at.fhooe.mc.android.mare.R;
 import at.fhooe.mc.android.mare.document.Document;
 
-public class EditorFragment extends Fragment {
+public class EditorFragment extends Fragment implements View.OnClickListener {
 
     private EditorViewModel editorViewModel;
 
     private Document mDocument;
-    private String mHeader;
-    private MarkdownEditText ed;
+    private MarkdownEditText mMDEditText;
 
     public View onCreateView(@NonNull LayoutInflater _inflater, ViewGroup _container, Bundle _savedInstanceState) {
         editorViewModel = ViewModelProviders.of(this).get(EditorViewModel.class);
@@ -34,17 +35,32 @@ public class EditorFragment extends Fragment {
         setHasOptionsMenu(false);
 
         mDocument = EditorActivity.mDocument;
-        ed = root.findViewById(R.id.fragment_editor_editText_editor);
+        mMDEditText = root.findViewById(R.id.fragment_editor_editText_editor);
         readFile(); // set text from file
         // (needs to be done before creating the MarkdownProcessor to get formatting properly)
 
         MarkdownProcessor markdownProcessor = new MarkdownProcessor(getContext());
         //markdownProcessor.config(markdownConfiguration);
         markdownProcessor.factory(EditFactory.create());
-        markdownProcessor.live(ed);
+        markdownProcessor.live(mMDEditText);
 
+        assignButtonListeners(root);
 
         return root;
+    }
+
+    private void assignButtonListeners(View root) {
+        root.findViewById(R.id.fragment_editor_buton_bold).setOnClickListener(this);
+        root.findViewById(R.id.fragment_editor_buton_italic).setOnClickListener(this);
+        root.findViewById(R.id.fragment_editor_buton_strikethrough).setOnClickListener(this);
+        root.findViewById(R.id.fragment_editor_buton_heading_add).setOnClickListener(this);
+        root.findViewById(R.id.fragment_editor_buton_heading_sub).setOnClickListener(this);
+        root.findViewById(R.id.fragment_editor_buton_ordered_list).setOnClickListener(this);
+        root.findViewById(R.id.fragment_editor_buton_unordered_list).setOnClickListener(this);
+        root.findViewById(R.id.fragment_editor_buton_horizontal_line).setOnClickListener(this);
+        root.findViewById(R.id.fragment_editor_buton_link).setOnClickListener(this);
+        root.findViewById(R.id.fragment_editor_buton_image).setOnClickListener(this);
+        root.findViewById(R.id.fragment_editor_buton_function).setOnClickListener(this);
     }
 
     @Override
@@ -72,14 +88,80 @@ public class EditorFragment extends Fragment {
 
     private void readFile() {
         String[] doc = mDocument.readFile();
-        mHeader = doc[0];
-        ed.setText(doc[1]);
+        mMDEditText.setText(doc[1]);
     }
 
 
     private void saveFile() {
         if (mDocument.getFile().exists() && // file hasn't been deleted
-                !mDocument.getContent().equals(ed.getText().toString())) // file has been changed
-            mDocument.saveFile(mDocument.getHeader().toString(), ed.getText().toString());
+                !mDocument.getContent().equals(mMDEditText.getText().toString())) // file has been changed
+            mDocument.saveFile(mDocument.getHeader().toString(), mMDEditText.getText().toString());
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int selStart = mMDEditText.getSelectionStart();
+        int selEnd = mMDEditText.getSelectionEnd();
+        Editable content = mMDEditText.getText();
+
+
+        Toast.makeText(getContext(), "start: " + selStart + ", end: " + selEnd, Toast.LENGTH_SHORT).show();
+
+        switch (v.getId()) {
+            case R.id.fragment_editor_buton_bold: {
+                format(content, selStart, selEnd, "**");
+
+                break;
+            }
+            case R.id.fragment_editor_buton_italic: {
+                format(content, selStart, selEnd, "*");
+                break;
+            }
+            case R.id.fragment_editor_buton_strikethrough: {
+                format(content, selStart, selEnd, "~~");
+                break;
+            }
+            case R.id.fragment_editor_buton_heading_add: {
+                Toast.makeText(getContext(), "h add", Toast.LENGTH_SHORT).show();
+
+                break;
+            }
+            case R.id.fragment_editor_buton_heading_sub: {
+                Toast.makeText(getContext(), "h sub", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+
+    }
+
+    private void format(Editable content, int selStart, int selEnd, String format) {
+        int len = format.length();
+//        if (selStart - len >= 0 && selEnd + len <= content.length())
+        try {
+
+            CharSequence st = content.subSequence(selStart - len, selStart);
+            CharSequence se = content.subSequence(selEnd, selEnd + len);
+            CharSequence c = content.subSequence(selStart, selEnd);
+
+            if (st.toString().equals(format) && se.toString().equals(format)) {
+                content.replace(selStart - len, selEnd + len, c);
+                mMDEditText.setSelection(selStart - len, selEnd - len);
+                return;
+
+            }
+
+
+            content.insert(selEnd, format);
+            content.insert(selStart, format);
+            mMDEditText.setSelection(selStart + len, selEnd + len);
+        } catch (IndexOutOfBoundsException e) {
+
+
+        }
+
     }
 }
